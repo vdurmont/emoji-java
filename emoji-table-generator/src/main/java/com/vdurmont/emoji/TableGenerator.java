666ac95d54bcf5e2d6113279c8487693db79bc86
Vincent DURMONT <vdurmont@gmail.com>
2014-10-07 20:48:29 -0700

    First implementation of a TableGenerator

---
+++ emoji-table-generator/src/main/java/com/vdurmont/emoji/TableGenerator.java
@@ -0,0 +1,55 @@
+package com.vdurmont.emoji;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * This app generate the emoji table in the README ;)
+ * <p/>
+ * Run with:
+ * mvn exec:java -Dexec.mainClass="com.vdurmont.emoji.TableGenerator"
+ */
+public class TableGenerator {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        // Table header
+        sb.append("| Emoji | Aliases |\n");
+        sb.append("| :---: | ------- |\n");
+
+        // Emojis!
+        for (Emoji emoji : EmojiManager.getAll()) {
+            String aliases = getAliases(emoji);
+            sb.append("| ")
+                    .append(emoji.getUnicode())
+                    .append(" | ")
+                    .append(aliases)
+                    .append(" |\n");
+        }
+
+        // Output!
+        if (args.length > 0) {
+            String path = args[0];
+            FileWriter writer = new FileWriter(path);
+            writer.write(sb.toString());
+            System.out.println("Written on " + path);
+        } else {
+            System.out.println(sb.toString());
+        }
+    }
+
+    private static String getAliases(Emoji emoji) {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for (String alias : emoji.getAliases()) {
+            if (first) {
+                first = false;
+            } else {
+                result.append(", ");
+            }
+            result.append(alias);
+        }
+
+        return result.toString();
+    }
+}
