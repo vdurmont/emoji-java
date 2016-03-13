d8589cbd8d365069f9061bafa4d7de688ddebd07
Vincent DURMONT <vdurmont@gmail.com>
2016-03-13 13:29:14 -0700

    Reorder imports

--- src/main/java/com/vdurmont/emoji/EmojiLoader.java
+++ src/main/java/com/vdurmont/emoji/EmojiLoader.java
@@ -3,15 +3,15 @@ package com.vdurmont.emoji;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 /**
  * Loads the emojis from a JSON database.
  *
