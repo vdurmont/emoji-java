c2fee100babe412f697baa090e110249bc461337
Vincent DURMONT <vdurmont@gmail.com>
2016-04-24 17:47:08 -0700

    Javadoc for EmojiParser.getEmojiEndPos

--- src/main/java/com/vdurmont/emoji/EmojiParser.java
+++ src/main/java/com/vdurmont/emoji/EmojiParser.java
@@ -337,6 +337,11 @@ public class EmojiParser {
    * Returns end index of a unicode emoji if it is found in text starting at index startPos, -1 if not found.
    * This returns the longest matching emoji, for example, in "\uD83D\uDC68\u200D\uD83D\uDC69\u200D\uD83D\uDC66"
    * it will find alias:family_man_woman_boy, NOT alias:man
+   *
+   * @param text the current text where we are looking for an emoji
+   * @param startPos the position in the text where we should start looking for an emoji end
+   *
+   * @return the end index of the unicode emoji starting at startPos. -1 if not found
    */
   protected static int getEmojiEndPos(char[] text, int startPos) {
     int best = -1;
