15cc1f86acba98ad33881471282f9f199b20cb92
Vincent DURMONT <vdurmont@gmail.com>
2016-05-22 11:06:35 -0700

    Fix the tests: some emojis have been added to the database but the test wasn't updated.

--- src/test/java/com/vdurmont/emoji/EmojiManagerTest.java
+++ src/test/java/com/vdurmont/emoji/EmojiManagerTest.java
@@ -130,7 +130,7 @@ public class EmojiManagerTest {
 
     // THEN
     // We know the number of distinct tags int the...!
-    assertEquals(567, tags.size());
+    assertEquals(594, tags.size());
   }
 
   @Test
